9e9afdbff507a63ef799662d7128ef9db80aa65d
Charlie on RohanElf <charlie@elvenware.com>
2016-10-25 16:43:30 -0700

    Elf Express Generator

--- templates/js/app.js
+++ templates/js/app.js
@@ -38,6 +38,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    'use strict';
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -49,12 +50,12 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    'use strict';
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;

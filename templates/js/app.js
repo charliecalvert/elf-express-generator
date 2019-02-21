9e48077ded7604a600c078daf663db1da91caf6f
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-02-20 16:08:51 -0800

    now listen

--- templates/js/app.js
+++ templates/js/app.js
@@ -45,7 +45,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-    app.use(function(err, req, res, next) {
+    app.use(function(err, req, res) {
         "use strict";
         res.status(err.status || 500);
         res.render("error", {

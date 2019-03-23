dabb18be8a382e18130426ee2d995f16c1aff4a6
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2019-03-23 12:24:48 -0700

    updating package.json and fixing template copies

--- bin/express-cli.js
+++ bin/express-cli.js
@@ -146,10 +146,16 @@ function createApplication (name, dir) {
             "build": "node_modules/.bin/webpack"
         },
         dependencies: {
+            "@babel/cli": "^7.2.3",
+            "@babel/plugin-proposal-class-properties": "^7.4.0",
             "debug": "~4.1.1",
             "express": "~4.16.4"
         },
         devDependencies: {
+            "@babel/core": "^7.4.0",
+            "@babel/preset-env": "^7.4.2",
+            "@babel/preset-react": "^7.0.0",
+            "babel-loader": "^8.0.5",
             "webpack": "^4.29.6",
             "webpack-cli": "^3.3.0"
         }
